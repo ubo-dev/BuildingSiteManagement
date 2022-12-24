d3fa874dd0da71e97e77e9cb3e183bad6f78e0b2
ubo-dev <umutokur11@outlook.com>
2022-12-24 18:11:49 +0300

    update

--- src/Database.java
+++ src/Database.java
@@ -1,60 +1,127 @@
 import java.sql.*;
+import java.util.Scanner;
+
 public class Database
 {
     private Statement statement = null;
     private ResultSet resultSet = null;
-    final String INSERT_PERSON = "INSERT INTO PERSON VALUES (?,?,?)";
-    final String INSERT_APARTMENT = "insert into Apartment values(?,?,?,?,?)";
-    final String INSERT_MANAGER = "insert into Manager values(?,?,?)";
-    final String INSERT_RESIDENT = "insert into Resident values(?,?,?,?,?)";
-    final String INSERT_ASSISTANTMANAGER = "insert into AssistantManager values(?,?,?,?)";
-    final String INSERT_CONTROLLER = "insert into Controller values(?,?,?)";
-    final String SELECT_LAST_PERSON = "SELECT TOP 1 * FROM Person ORDER BY personID DESC";
-    final String SELECT_LAST_RESIDENT = "SELECT TOP 1 * FROM Resident ORDER BY residentID DESC";
-    final String SELECT_LAST_MANAGER = "SELECT TOP 1 * FROM Manager ORDER BY managerID DESC";
+    private final String INSERT_APARTMENT = "insert into Apartment values(?,?,?,?,?)";
+    private final String INSERT_MANAGER = "insert into Manager values(?,?,?,?)";
+    private final String INSERT_ASSISTANTMANAGER = "insert into AssistantManager values(?,?,?,?)";
+    private final String INSERT_CONTROLLER = "insert into Controller values(?,?,?)";
+    private final String INSERT_RESIDENT = "insert into Resident values(?,?,?,?,?)";
+    private final String SELECT_LAST_RESIDENT = "SELECT TOP 1 * FROM Resident ORDER BY residentID DESC";
+    private final String SELECT_LAST_Manager = "SELECT TOP 1 * FROM Manager ORDER BY managerID DESC";
+    private final String SELECT_LAST_CASHREGISTER = "SELECT TOP 1 * FROM CashRegister ORDER BY transactionID DESC";
+
+
+
+
+    public String getINSERT_APARTMENT() {
+        return INSERT_APARTMENT;
+    }
+
+    public String getINSERT_PERSON() {
+        return "INSERT INTO PERSON VALUES (?,?,?)";
+    }
     public Connection getConnection(String url) throws SQLException {
         Connection connection = DriverManager.getConnection(url);
         connection.setAutoCommit(true);
         return connection;
     }
 
-    public void insertIntoPerson(Connection connection,PreparedStatement psInsert,String personName,int apartmentID,int status,String residentPhone,int paidFlag)
+    public void deleteTable(Connection connection,String tablename, String con3,String con4)
+    {
+        String SQL_DELETE = "DELETE FROM "+tablename+" WHERE "+con3+"='"+con4+"'";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_DELETE);
+            preparedStatement.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateTable(Connection connection,String tableName, String con1,String con2,String con3,String con4)
+    {
+        String SQL_UPDATE = "UPDATE "+tableName+" SET "+con1+"='"+con2+"' WHERE "+con3+"='"+con4+"'";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_UPDATE);
+            preparedStatement.execute();
+
+           /* if (con2.equals("PersonStatus"))
+            {
+                if (con3.equals("1") && con4.equals("2"))
+                {
+                    //insert into asistant manager
+                    PreparedStatement psInsert = connection.prepareStatement(INSERT_ASSISTANTMANAGER);
+                    insertIntoAssistantManager(connection);
+                }
+                else if (con3.equals("1") && con4.equals("3"))
+                {
+                    deleteTable(connection,tableName,con3,con4);
+                    PreparedStatement psInsert = connection.prepareStatement(INSERT_CONTROLLER);
+                    insertIntoAssistantManager(connection);
+                }
+                else if (con3.equals("2") && con4.equals("1"))
+                {
+                    deleteTable(connection,tableName,con3,con4);
+                }
+                else if (con3.equals("2") && con4.equals("3"))
+                {
+                    deleteTable(connection,tableName,con3,con4);
+                    PreparedStatement psInsert = connection.prepareStatement(INSERT_CONTROLLER);
+                    insertIntoController(connection);
+                }
+                else if (con3.equals("3") && con4.equals("1"))
+                {
+                    deleteTable(connection,tableName,con3,con4);
+                    PreparedStatement psInsert = connection.prepareStatement(INSERT_MANAGER);
+                    insertIntoManager(connection);
+                }
+                else if (con3.equals("3") && con4.equals("2"))
+                {
+                    deleteTable(connection,tableName,con3,con4);
+                    PreparedStatement psInsert = connection.prepareStatement(INSERT_ASSISTANTMANAGER);
+                    insertIntoAssistantManager(connection);
+                }
+
+            }*/
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insertIntoPerson(Connection connection,PreparedStatement psInsert,int apartmentID,String personName,int status,String residentPhone,int paidFlag)
     {
         try {
-            psInsert.setString(1,personName);
-            psInsert.setInt(2,apartmentID);
+            psInsert.setInt(1,apartmentID);
+            psInsert.setString(2,personName);
             psInsert.setInt(3,status);
-
+            psInsert.execute();
 
             if (status != 4)
             {
-                PreparedStatement psInsertResident = connection.prepareStatement(INSERT_RESIDENT);
-                insertIntoResident(connection,psInsertResident,apartmentID, personName,residentPhone,paidFlag);
+                insertIntoResident(connection,apartmentID, personName,residentPhone,paidFlag);
                 if (status == 1) //manager
                 {
-                    PreparedStatement psInsertManager = connection.prepareStatement(INSERT_MANAGER);
-                    insertIntoManager(connection,psInsertManager);
+                    insertIntoManager(connection,apartmentID,personName,status);
                 }
                 else if (status == 2) //assistant manager
                 {
-                    PreparedStatement psInsertManager = connection.prepareStatement(INSERT_MANAGER);
-                    insertIntoManager(connection,psInsertManager);
-                    PreparedStatement psInsertAsistantManager = connection.prepareStatement(INSERT_ASSISTANTMANAGER);
-                    insertIntoAssistantManager(connection,psInsertAsistantManager);
+                    insertIntoManager(connection,apartmentID,personName,status);
                 }
                 else if (status == 3) //controller
                 {
-                    PreparedStatement psInsertManager = connection.prepareStatement(INSERT_CONTROLLER);
-                    insertIntoController(connection,psInsertManager);
+                    insertIntoController(connection,personName);
                 }
             }
             else
             {
-                PreparedStatement psInsertResident = connection.prepareStatement(INSERT_RESIDENT);
-                insertIntoResident(connection,psInsertResident,apartmentID, personName,residentPhone,paidFlag);
+                insertIntoResident(connection,apartmentID, personName,residentPhone,paidFlag);
             }
 
-            psInsert.execute();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -78,96 +145,182 @@ public class Database
 
     }
 
-    public void insertIntoResident(Connection connection,PreparedStatement psInsert,int apartmentID,String residentName,String residentPhone,int paidFlag)
+    public void insertIntoResident(Connection connection,int apartmentID,String residentName,String residentPhone,int paidFlag)
     {
         try {
+            PreparedStatement psInsertResident = connection.prepareStatement(INSERT_RESIDENT);
             statement = connection.createStatement();
+            String SELECT_LAST_PERSON = "SELECT TOP 1 * FROM Person ORDER BY personID DESC";
             resultSet = statement.executeQuery(SELECT_LAST_PERSON);
             int personID = 0;
 
             while (resultSet.next())
                 personID = resultSet.getInt("personID");
 
-            psInsert.setInt(1,personID);
-            psInsert.setInt(2,apartmentID);
-            psInsert.setString(3,residentName);
-            psInsert.setString(4,residentPhone);
-            psInsert.setInt(5,paidFlag);
-            psInsert.execute();
+            psInsertResident.setInt(1,personID);
+            psInsertResident.setInt(2,apartmentID);
+            psInsertResident.setString(3,residentName);
+            psInsertResident.setString(4,residentPhone);
+            psInsertResident.setInt(5,paidFlag);
+            psInsertResident.execute();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
     }
 
-    public void insertIntoManager(Connection connection,PreparedStatement psInsert)
+    public void insertIntoManager(Connection connection,int apartmentID,String managerName,int status)
     {
         try {
-            int personID = 0;
-            int residentID = 0;
-            String managerName = null;
+            PreparedStatement psInsertManager = connection.prepareStatement(INSERT_MANAGER);
             statement = connection.createStatement();
             resultSet = statement.executeQuery(SELECT_LAST_RESIDENT);
+            int personID = 0;
+            int residentID = 0;
             while (resultSet.next()) {
                 personID = resultSet.getInt("personID");
                 residentID = resultSet.getInt("residentID");
-                managerName = resultSet.getString("residentName");
             }
-            psInsert.setInt(1,personID);
-            psInsert.setInt(2,residentID);
-            psInsert.setString(3,managerName);
-            psInsert.execute();
+
+            psInsertManager.setInt(1,personID);
+            psInsertManager.setInt(2,apartmentID);
+            psInsertManager.setInt(3,residentID);
+            psInsertManager.setString(4,managerName);
+            psInsertManager.execute();
+
+            if (status == 2) insertIntoAssistantManager(connection,personID,residentID,managerName);
+
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void insertIntoAssistantManager(Connection connection,PreparedStatement psInsert)
+    public void insertIntoAssistantManager(Connection connection,int personID,int residentID,String managerName)
     {
         try {
+            PreparedStatement psInsertAsistantManager = connection.prepareStatement(INSERT_ASSISTANTMANAGER);
             statement = connection.createStatement();
-            resultSet = statement.executeQuery(SELECT_LAST_MANAGER);
+            resultSet = statement.executeQuery(SELECT_LAST_Manager);
             int managerID = 0;
-            int personID = 0;
-            int residentID = 0;
-            String managerName = null;
             while (resultSet.next()) {
                 managerID = resultSet.getInt("managerID");
-                personID = resultSet.getInt("personID");
-                residentID = resultSet.getInt("residentID");
-                managerName = resultSet.getString("managerName");
             }
-            psInsert.setInt(1,personID);
-            psInsert.setInt(2,managerID);
-            psInsert.setInt(3,residentID);
-            psInsert.setString(4,managerName);
-            psInsert.execute();
+            psInsertAsistantManager.setInt(1,personID);
+            psInsertAsistantManager.setInt(2,managerID);
+            psInsertAsistantManager.setInt(3,residentID);
+            psInsertAsistantManager.setString(4,managerName);
+            psInsertAsistantManager.execute();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void insertIntoController(Connection connection,PreparedStatement psInsert)
+    public void insertIntoController(Connection connection,String controllerName)
     {
         try {
+            PreparedStatement psInsertController = connection.prepareStatement(INSERT_CONTROLLER);
             statement = connection.createStatement();
             resultSet = statement.executeQuery(SELECT_LAST_RESIDENT);
             int personID = 0;
             int residentID = 0;
-            String controllerName = null;
             while (resultSet.next()) {
                 personID = resultSet.getInt("personID");
                 residentID = resultSet.getInt("residentID");
-                controllerName = resultSet.getString("residentName");
             }
-            psInsert.setInt(1,personID);
-            psInsert.setInt(2,residentID);
-            psInsert.setString(3,controllerName);
-            psInsert.execute();
+            psInsertController.setInt(1,personID);
+            psInsertController.setInt(2,residentID);
+            psInsertController.setString(3,controllerName);
+            psInsertController.execute();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
+    public void insertIntoCashRegister(Connection connection,float totalIncomingMoney,float totalExpenseMoney,String transactionDate,int controllerID,int residentID,String paymentDescription,int isMonthlyFLag)
+    {
+        try {
+
+            String INSERT_CASHREGISTER = "insert into CashRegister values(?,?,?)";
+            PreparedStatement psInsert = connection.prepareStatement(INSERT_CASHREGISTER);
+            psInsert.setFloat(1,totalIncomingMoney);
+            psInsert.setFloat(2,totalExpenseMoney);
+            psInsert.setString(3,transactionDate);
+            psInsert.execute();
+
+            System.out.println("Is it a payment?(y or n)");
+            Scanner scanner = new Scanner(System.in);
+            String choice = scanner.next();
+            if (choice.equals("y"))
+            {
+                insertIntoPayment(connection,controllerID,residentID,paymentDescription,isMonthlyFLag);
+            }
+            else
+                insertIntoExpense(connection,controllerID,paymentDescription,isMonthlyFLag);
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insertIntoPayment(Connection connection,int controllerID,int residentID,String paymentDescription,int isMonthlyFlag)
+    {
+        try {
+
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(SELECT_LAST_CASHREGISTER);
+            int transactionID = 0;
+            float totalIncomingMoney = 0;
+            String transactionDate = null;
+            while (resultSet.next()) {
+                transactionID = resultSet.getInt("transactionID");
+                totalIncomingMoney = resultSet.getFloat("totalIncomingMoney");
+            }
+
+            String INSERT_PAYMENT = "insert into Payment values(?,?,?,?,?,?)";
+            PreparedStatement psInsert = connection.prepareStatement(INSERT_PAYMENT);
+            psInsert.setInt(1,transactionID);
+            psInsert.setInt(2,controllerID);
+            psInsert.setInt(3,residentID);
+            psInsert.setFloat(4,totalIncomingMoney);
+            psInsert.setString(5,paymentDescription);
+            psInsert.setInt(6,isMonthlyFlag);
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insertIntoExpense(Connection connection,int controllerID,String expenseDescription,int isMonthlyFlag)
+    {
+        try {
+
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(SELECT_LAST_CASHREGISTER);
+            int transactionID = 0;
+            float totalExpenseMoney = 0;
+            while (resultSet.next()) {
+                transactionID = resultSet.getInt("transactionID");
+                totalExpenseMoney = resultSet.getFloat("totalExpenseMoney");
+            }
+
+            String INSERT_EXPENSE = "insert into Expense values(?,?,?,?,?)";
+            PreparedStatement psInsert = connection.prepareStatement(INSERT_EXPENSE);
+            psInsert.setInt(1,transactionID);
+            psInsert.setFloat(2,totalExpenseMoney);
+            psInsert.setInt(3,controllerID);
+            psInsert.setString(4,expenseDescription);
+            psInsert.setInt(5,isMonthlyFlag);
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     public void closeConnection (Connection connection)
     {
         try {
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import java.net.ConnectException;
 import java.sql.*;
 
 public class Main {
-    private static final String SQL_UPDATE = "UPDATE ? SET ? = ? WHERE ? = ?";
-    private static final String SQL_DELETE = "delete ? from ? where ? = ?";
-
     public static void main(String[] args) {
         final String QUERY = "select * from Person";
         final String serverName = "UBO";
@@ -14,46 +11,44 @@ public class Main {
         Connection connection = null;
         Statement statement = null;
         PreparedStatement psInsert = null;
-        PreparedStatement psUpdate = null;
-        PreparedStatement psDelete = null;
         ResultSet resultSet = null;
 
 
         try{
             connection = db.getConnection(conUrl);
             System.out.println("connected");
-            psInsert = connection.prepareStatement(db.INSERT_PERSON);
-
+            psInsert = connection.prepareStatement(db.getINSERT_APARTMENT());
             //inserting new apartment
-            db.insertIntoApartment(connection,"Deniz","Bestekar","Ankara","Cankaya","06510");
+            //db.insertIntoApartment(connection,"Deniz","Bestekar","Ankara","Cankaya","06510");
+            //db.insertIntoApartment(connection,"Beyaz Saray","Ayvalı","Ankara","Keçiören","06780");
 
             // inserting persons
-            // status 1 == manager | 2 == assistant manager | 3 == controller | 4 == regular resident
-            db.insertIntoPerson(connection,psInsert,"umut",2,1,"+905639453868",1);
-            db.insertIntoPerson(connection,psInsert,"mert",3,2,"+905229457431",1);
-            db.insertIntoPerson(connection,psInsert,"hilal",4,3,"+905639453868",1);
-            db.insertIntoPerson(connection,psInsert,"fatih",5,4,"+905133563961",0);
+            // status 1 == manager | 2 == assistant manager | 3 == controller | 4 == regular resident*/
+            psInsert = connection.prepareStatement(db.getINSERT_PERSON());
+            db.insertIntoPerson(connection,psInsert,1,"Umut Okur",1,"+905639453868",1);
+            db.insertIntoPerson(connection,psInsert,1,"Mert Urhan",2,"+905229457431",1);
+            db.insertIntoPerson(connection,psInsert,2,"Hilal Kabanlı",3,"+905639453868",1);
+            db.insertIntoPerson(connection,psInsert,2,"Fatih Nar",4,"+905133563961",0);
+
+            //db.insertIntoCashRegister(connection,150,0,"2022-10-14",8,35,"due",1);
+            //db.insertIntoCashRegister(connection,0,55000,"2022-11-20",6,25,"salary",1);
+
+            //deletion
+            db.deleteTable(connection,"Apartment","apartmentName","Deniz");
+            //db.deleteTable(connection,"Apartment","apartmentName","Beyaz Saray");
 
 
+            //update
+            //db.updateTable(connection,"Resident","residentPhoneNum","+905444399868","residentName","Mert Urhan");
+            //db.updateTable(connection,"Person","PersonStatus","1","PersonName","Hilal Kabanlı");
 
 
-            statement = connection.createStatement();
-            statement.execute(QUERY);
-            resultSet = statement.getResultSet();
-
-            while (resultSet.next()) {
-                int personID = resultSet.getInt(1);
-                String personName = resultSet.getString(2);
-                int apartmentID = resultSet.getInt(3);
-                int personStatus = resultSet.getInt(4);
-                System.out.println(personID + " " + personName + " " + apartmentID + " " + personStatus);
-            }
+            //
 
         } catch (Exception e) {
             System.out.println("Failed...");
             e.printStackTrace();
         } finally {
-            db.closeStatement(statement);
             db.closeConnection(connection);
             System.out.println();
             System.out.println("Disconnected...");
